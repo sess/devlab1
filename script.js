9093a39ea40ba0d3c0c2e749721e3125f13b576d
sergio <serj.sanch@gmail.com>
2018-07-11 20:13:52 +0000

    index.html redone

--- script.js
+++ script.js
@@ -1,47 +1,32 @@
 /* global $ */
 
 $(document).ready(function () {
-	
-	function underline(a,b,c,d){
-		$(a).css('text-decoration','none');
-		$(b).css('text-decoration','none');
-		$(c).css('text-decoration','none');
-		$(d).css('text-decoration','underline');	
-	}
-
-	$('#homelink').click(function () {
-		$('#mainimg').show();
-		$('#aboutmepage').hide();
-		$('.contactcontent').hide();
-		$('.contact').hide();
-		$('#portfoliopage').hide();
-		underline('#aboutmelink','#portfoliolink','#contactlink','#homelink');
-	});
-
-	$('#aboutmelink').click(function () {
-		$('#mainimg').hide();
-		$('.contactcontent').hide();
-		$('#aboutmepage').show();
-		$('.contact').hide();
-		$('#portfoliopage').hide();
-		underline('#homelink','#contactlink','#portfoliolink','#aboutmelink');
-	});
-
-	$('#portfoliolink').click(function () {
-		$('#mainimg').hide();
-		$('.contactcontent').hide();
-		$('#aboutmepage').hide();
-		$('#portfoliopage').show();
-		$('.contact').hide();
-		underline('#homelink','#aboutmelink','#contactlink','#portfoliolink');
-	});
-
-	$('#contactlink').click(function () {
-		$('#mainimg').hide();
-		$('#aboutmepage').hide();
-		$('.contactcontent').show();
-		$('.contact').hide();
-		$('#portfoliopage').hide();
-		underline('#homelink','#aboutmelink','#portfoliolink','#contactlink');
-	});
+    
+    
+    $('#option1').click(function(){
+        $('.options').css('background-color','#262626');
+        $('#option1').css('background-color','black');
+        $('.pages').hide();
+        $('#aboutmepage').show();
+   });
+   
+   $('#option2').click(function(){
+        $('.options').css('background-color','#262626');
+        $('#option2').css('background-color','black');
+        $('.pages').hide();
+        $('#myworkpage').show();
+   });
+   
+   $('select').click(function(){
+       var option = $('select').val();
+       if(option === 'vm'){
+            $('#sampleimg').attr('src','/sess/devlab1/images/vending_machine_screenshot.PNG');
+            $('#description').text('A very basic virtual vending machine.' + 'This was a project for the ScriptEd class I\'m in, I worked on it both in class and at my house. I did edit it quite a bit it afterwards to make it look more appealing.');
+            $('#tryout').attr('href','/sess/devlab1/vm/vending_machine.html');
+       }else if(option === "hh"){
+            $('#sampleimg').attr('src','/sess/devlab1/images/hats_hoods_screenshot.PNG');
+            $('#description').text('(Incomplete) A website for a fictitious company. I am making this purely for practice.');
+            $('#tryout').attr('href','/sess/devlab1/h&h/h&h.html');
+       }
+   });
 });
