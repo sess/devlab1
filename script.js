dd57dc443ebedb7f0ea8e1885913b5b747d7f3af
sergio <serj.sanch@gmail.com>
2018-07-14 01:36:15 +0000

    Styled index.html

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 
 $(document).ready(function () {
     
+    $('#option0').click(function(){
+        $('.options').css('background-color','#262626');
+        $('#option0').css('background-color','black');
+        $('.pages').hide();
+        $('#homepage').show();
+   });
     
     $('#option1').click(function(){
         $('.options').css('background-color','#262626');
@@ -17,11 +23,18 @@ $(document).ready(function () {
         $('#myworkpage').show();
    });
    
+   $('#option3').click(function(){
+        $('.options').css('background-color','#262626');
+        $('#option3').css('background-color','black');
+        $('.pages').hide();
+        $('#contactpage').show();
+   });
+   
    $('select').click(function(){
        var option = $('select').val();
        if(option === 'vm'){
             $('#sampleimg').attr('src','/devlab1/images/vending_machine_screenshot.PNG');
-            $('#description').text('A very basic virtual vending machine.' + 'This was a project for the ScriptEd class I\'m in, I worked on it both in class and at my house. I did edit it quite a bit it afterwards to make it look more appealing.');
+            $('#description').text('A very basic virtual vending machine.' + ' This was a project for the ScriptEd class I\'m in, I worked on it both in class and at my house. I did edit it quite a bit it afterwards to make it look more appealing.');
             $('#tryout').attr('href','/devlab1/vm/vending_machine.html');
        }else if(option === "hh"){
             $('#sampleimg').attr('src','/devlab1/images/hats_hoods_screenshot.PNG');
