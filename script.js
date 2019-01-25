8279e60404beae248d8fa9a515b5fa2a69f5a210
sergio <serj.sanch@gmail.com>
2019-01-25 22:57:34 +0000

    fixed images

--- script.js
+++ script.js
@@ -37,19 +37,19 @@ $(document).ready(function () {
    $('select').click(function(){
        var option = $('select').val();
        if(option === 'cts'){
-            $('#sampleImg').attr('src','/sampleImgs/crackthesafeSample.png');
+            $('#sampleImg').attr('src','crackthesafeSample.png');
             $('.sampleBlurb').text('A pretty simple game that I created as part of a project for the Code Nation Advanced Course. The goal of the game is to try and guess the passcode in order to unlock the safe.');
             $('#tryout').attr('href',"https://sess.github.io/crackthesafe/");
        }else if(option === "cfp"){
-            $('#sampleImg').attr('src','/sampleImgs/crackthesafeSample.png');
+            $('#sampleImg').attr('src','cerealfanpageSample.png');
             $('.sampleBlurb').text('A simple fanpage about cereal. I created it as part of the Code Nation Advanced Course.');
             $('#tryout').attr('href','https://sess.github.io/fanpage/');
        }else if(option === "ttt"){
-           $("#sampleImg").attr("src","/sampleImgs/tictactoeSample.png");
+           $("#sampleImg").attr("src","tictactoeSample.png");
            $(".sampleBlurb").text("Recreated tic tac toe as part of the Code Nation Advanced Course.");
            $("#tryout").attr("href","https://sess.github.io/tictactoe/");
        }else if(option === "pl"){
-           $("#sampleImg").attr("src","/sampleImgs/playlistSample.PNG");
+           $("#sampleImg").attr("src","playlistSample.PNG");
            $(".sampleBlurb").text("As part of the Code Nation Advanced Course, I made a sort of playlist in order to demonstrate \".forEach();\", \".push\", and so on");
            $("#tryout").attr("href","https://sess.github.io/playlist/");
        }
